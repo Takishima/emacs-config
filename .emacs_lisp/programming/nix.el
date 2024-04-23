d9439e991bd8cb7cc94518020f929b8d92b509a4
Damien Nguyen <ngn.damien@gmail.com>
2024-04-23 21:36:56 +0200

    Fix issues with lsp-nix

--- .emacs_lisp/programming/nix.el
+++ .emacs_lisp/programming/nix.el
@@ -1,14 +1,14 @@
 (use-package nixpkgs-fmt
   :straight t)
 
-(use-package lsp-nix
-  ;; :straight t
-  :ensure lsp-mode
-  :after (lsp-mode)
-  :demand t
-  ;; :custom
-  ;; (lsp-nix-nil-formatter ["alejandra"])
-  )
+;; (use-package lsp-nix
+;;   :straight t
+;;   :ensure nil
+;;   :after (lsp-mode)
+;;   :demand t
+;;   ;; :custom
+;;   ;; (lsp-nix-nil-formatter ["alejandra"])
+;;   )
 
 (use-package nix-mode
   :straight t
